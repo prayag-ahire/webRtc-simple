c09316645728da329143c812bc555277dd9d6e4d
prayag <prayagahire365@gmail.com>
2025-02-25 22:30:01 +0530

    backend error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,37 @@
-import { WebSocketServer } from "ws";
-import express from "express"
+import { WebSocket, WebSocketServer } from "ws";
 
-const app = express();
-const port = 8080;
+const wss = new WebSocketServer({port:8080});
 
-const server = app.listen(port,()=>{
-  console.log("server is listening....")
+let senderSocket: null|WebSocket = null;
+let receiverSocket: null | WebSocket = null;
+
+wss.on('connection',function connection(ws){
+    ws.on('error',console.error);
+
+    ws.on('message',function message(data:Buffer){
+        const message = JSON.parse(data.toString());
+    if (message.type === 'sender') {
+      senderSocket = ws;
+    } else if (message.type === 'receiver') {
+      receiverSocket = ws;
+    } else if (message.type === 'createOffer') {
+      if (ws !== senderSocket) {
+        return;
+      }
+      receiverSocket?.send(JSON.stringify({ type: 'createOffer', sdp: message.sdp }));
+    } else if (message.type === 'createAnswer') {
+        if (ws !== receiverSocket) {
+          return;
+        }
+        senderSocket?.send(JSON.stringify({ type: 'createAnswer', sdp: message.sdp }));
+    } else if (message.type === 'iceCandidate') {
+      if (ws === senderSocket) {
+        receiverSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+      } else if (ws === receiverSocket) {
+        senderSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+      }
+    }
+  });
 });
 
-const  wss = new WebSocketServer({server});
-
-wss.on("connection",(ws)=>{
-  ws.on("message",(data:any)=>{
-    console.log("data form client",data);
-    ws.send("thanks buddy!")
-  })
-})
+ 
